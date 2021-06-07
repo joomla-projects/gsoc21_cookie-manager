53eab2c6330455b8357669c214861b560f4594e7
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-07 15:07:39 +0530

    Add initial folders

--- administrator/components/com_cookiemanager/tmpl/cookiemanagers/default.php
+++ administrator/components/com_cookiemanager/tmpl/cookiemanagers/default.php
@@ -1,10 +1,1 @@
-<?php
 
-defined('_JEXEC') or die;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\CMS\Router\Route;
-
- ?>
-<h1>Rishabh</h1>
